f921d76cd619145b3b9925c328f6c0f7b2a0c0d0
Thomas Passos <tm123br@live.com>
2025-05-17 20:05:41 -0300

    feat: criação de mais arquivos de testes e organização de pastas de testes

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 import logging
-import traceback
 
 from dotenv import load_dotenv
 from flask import Flask
 from flask_talisman import Talisman
-from loguru import logger
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from config import Config
@@ -13,48 +11,42 @@ from config import Config
 def create_app(config_object=Config):
     load_dotenv()
 
-    try:
-        app = Flask(__name__)
-        if config_object:
-            app.config.from_object(config_object)
+    app = Flask(__name__)
+    if config_object:
+        app.config.from_object(config_object)
 
-        from app.external.logging import InterceptHandler
+    from app.external.logging import InterceptHandler
 
-        app.logger.handlers = [InterceptHandler()]
-        if not app.debug:
-            app.logger.setLevel(logging.INFO)
+    app.logger.handlers = [InterceptHandler()]
+    if not app.debug:
+        app.logger.setLevel(logging.INFO)
 
-        app.wsgi_app = ProxyFix(
-            app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1
-        )
-        # Inicialização das extensões
-        from app.external.models import db, migrate
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1
+    )
+    # Inicialização das extensões
+    from app.external.models import db, migrate
 
-        db.init_app(app)
-        migrate.init_app(app, db)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-        from app.external.cache import cache
+    from app.external.cache import cache
 
-        cache.init_app(app)
+    cache.init_app(app)
 
-        from app.external.schemas import ma
+    from app.external.schemas import ma
 
-        ma.init_app(app)
+    ma.init_app(app)
 
-        # Inicialização do Celery
-        import app.external.celery as cl
+    # Inicialização do Celery
+    import app.external.celery as cl
 
-        cl.init_app(app)
+    cl.init_app(app)
 
-        # Registro de blueprints
-        import app.blueprints as bp
+    # Registro de blueprints
+    import app.blueprints as bp
 
-        bp.init_app(app)
+    bp.init_app(app)
 
-        Talisman(app, force_https=False, content_security_policy=None)
-        return app
-    except Exception as e:
-        logger.critical(
-            f"Factory não retornando a aplicação:{e}\n{traceback.format_exc()}"
-        )
-        return None
+    Talisman(app, force_https=False, content_security_policy=None)
+    return app
